d3eed475c050b2c6d34372c04d1f0d87d12fa906
Unknown <j.brunn@hotmail.com>
2018-06-13 22:39:04 +0100

    Initial Unity commit (Recoil/Ninja)

---
+++ AnimationShowcase/Assets/Particles/FireGun.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particle;
+    [SerializeField] private Transform barrelLocation;
+
+    public void PlayParticle()
+    {
+        var part = Instantiate(particle, barrelLocation.position, Quaternion.identity, barrelLocation);
+        part.transform.forward = barrelLocation.TransformDirection(Vector3.forward);
+    }
+}
---
+++ AnimationShowcase/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField] bool lerpToSubject;
+    [SerializeField] float speed;
+    [SerializeField] GameObject subject;
+
+    void Update()
+    {
+        if (lerpToSubject)
+        {
+            Quaternion originalRot = transform.rotation;
+            transform.LookAt(subject.transform.position);
+            Quaternion newRot = transform.rotation;
+            transform.rotation = Quaternion.Lerp(originalRot, newRot, Time.deltaTime * speed);
+
+        }
+        else
+        {
+            transform.LookAt(subject.transform.position);
+        }
+    }
+}
